12b7f48f0bc352ec1f58593e747e69e36325546f
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-04-02 14:07:16 +0200

    Initial commit

---
+++ src/main/java/com/moldavets/ecom_store/EcomStoreApplication.java
@@ -0,0 +1,13 @@
+package com.moldavets.ecom_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moldavets/ecom_store/EcomStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldavets.ecom_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
